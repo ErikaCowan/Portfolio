d94a87dccf32185cca0e6ba5774d3eb99522c79a
erikacowan <herbalgirlskincare@gmail.com>
2025-05-21 17:04:47 -0500

    update

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,40 +1,38 @@
 $(document).ready(function(){
 
+    // 1) FILTER / GROUPING
     $(".button").click(function(){
-        $(this).addClass("active").siblings().removeClass("active");
-
-        var filter = $(this).attr("data-filter");
-    
-        if (filter == "all"){
-            $(".gallery .image").show(400);
-        }
-        else{
-            $(".gallery .image").not("."+filter).hide(200);
-            $(".gallery .image").filter("."+filter).show(400);
-        }
-    })
-
-    //MAGNIFIC-POPUP
+      // toggle active class
+      $(this).addClass("active").siblings().removeClass("active");
+  
+      var filter = $(this).attr("data-filter");
+  
+      if (filter === "all") {
+        $(".gallery .image").show(400);
+      } else {
+        $(".gallery .image").not("." + filter).hide(200);
+        $(".gallery .image").filter("." + filter).show(400);
+      }
+    });
+  
+    // 2) MAGNIFIC-POPUP (only on your preview thumbnails)
     $(".gallery").magnificPopup({
-        
-        delegate: "a",
-        type: "image",
-        removalDelay: 500, //delay removal by X to allow out-animation
-        gallery:{
-            enabled: true
-        },
-
-        callbacks: {
-            beforeOpen: function() {
-              // just a hack that adds mfp-anim class to markup 
-               this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-               this.st.mainClass = this.st.el.attr('data-effect');
-            }
-          },
-          closeOnContentClick: true,
-          midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-
-    })
-
-
-});
+      delegate: "a.view-btn",      // <-- only links with .view-btn
+      type: "image",
+      removalDelay: 500,           // allow out-animation
+      gallery: { enabled: true },
+  
+      callbacks: {
+        beforeOpen: function() {
+          // add mfp-with-anim class for your animations
+          this.st.image.markup = this.st.image.markup
+            .replace('mfp-figure', 'mfp-figure mfp-with-anim');
+          this.st.mainClass = this.st.el.attr("data-effect");
+        }
+      },
+  
+      closeOnContentClick: true,
+      midClick: true  // open on middle mouse click
+    });
+  
+  });
